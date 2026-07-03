52a7ab54c7fd80ad8aec86da682ec0a20e7d0a8f
KleimenovM <maksim.kleimenov@polytechnique.edu>
2026-07-03 16:42:08 +0200

    biteau catalog analyzed

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,9 @@
+# /config/settings.py
+# Configuration settings for the project
+
 from pathlib import Path
 
-
 ROOT_DIR = Path(__file__).resolve().parent.parent
+
+DATA_DIR = ROOT_DIR / "data"
+CATALOG_DIR = DATA_DIR / "catalog_files"
